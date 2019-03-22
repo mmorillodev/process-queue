a64a9025c166f328a8dd846998c15e889701240c
brdnescara_matheus <matheus.morillo@nescara.com>
2019-03-22 10:13:56 -0300

    all commited

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
 	public static void main(String[] args) {
-		Queue<Process> q = new Queue<>();
+		Queue<Process> q = new Queue<Process>();
 		Process p1 = new Process("Process 1", 0);
 		q.add(p1);
 		q.add(new Process("Process 2", 1));
