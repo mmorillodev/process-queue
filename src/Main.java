bcda324cac09de6ffdd177fa3ac4a468f8a0bc1b
brdnescara_matheus <matheus.morillo@nescara.com>
2019-03-25 10:07:46 -0300

    Process.java constructor receives a file path to construct all its attributes

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,19 @@
+import java.io.IOException;
 
 public class Main {
-	public static void main(String[] args) {
-		Queue<Process> q = new Queue<Process>();
-		Process p1 = new Process("Process 1", 0);
-		q.add(p1);
-		q.add(new Process("Process 2", 1));
-		q.add(new Process("Process 3", 0));
-		q.add(new Process("Process 4", 2));
-		
-		System.out.println(q.unQueueByPriority().getName());
-		System.out.println(q.toString());
-		System.out.println(q.unQueueByPriority().getName());
-		System.out.println(q.toString());
-		System.out.println(q.unQueueByPriority().getName());
-		System.out.println(q.toString());
-		System.out.println(q.unQueueByPriority().getName());
-		
-		System.out.println(q.toString());
+	private static final String basePath = "C:\\Users\\Nescara\\Documents\\";
+	public static void main(String[] args) throws IOException {
+		Queue<Process> wait = new Queue<>();
+		Queue<Process> ready = new Queue<>();
+	
+	}
+	
+	public static void sleep(long millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- src/Process.java
+++ src/Process.java
@@ -1,16 +1,67 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import aux_interfaces.Prioriser;
 
 public class Process implements Prioriser{
 	private String name;
 	private int priority;
+	private boolean hasIO;
+	private int[] ioArrivals;
+	private int duration, arrival;
 	
-	public Process(String name){
-		this.name = name;
+	public Process(String filePath) throws IOException{
+		FileReader file = new FileReader(filePath);
+		BufferedReader buffer = new BufferedReader(file);
+		ArrayList<String> fileValues = new ArrayList<>();
+		String line;
+		
+		while((line = buffer.readLine()) != null) {
+			fileValues.add(line);
+		}
+		buffer.close();
+		
+		setName(fileValues.get(0));
+		setDuration(Integer.parseInt(fileValues.get(1)));
+		setArrival(Integer.parseInt(fileValues.get(2)));
+		hasIO(fileValues.get(3).equalsIgnoreCase("SIM") ? true : false);
+		if(hasIO()) {
+			setIOIntervals(toIntArray(fileValues.get(4).split(" ")));
+		}
+		else
+			ioArrivals = new int[0];
+	}
+	
+	public void hasIO(boolean hasIO) {
+		this.hasIO = hasIO;
+	}
+	
+	public boolean hasIO() {
+		return hasIO;
+	}
+	
+	public void setArrival(int arrival) {
+		this.arrival = arrival;
+	}
+	
+	public void setIOIntervals(int[] intervals) {
+		if(hasIO)
+			this.ioArrivals = intervals;
 	}
 	
 	public Process(String name, int priority){
 		this.name = name;
 		this.priority = priority;
+	}
+	
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	
+	public double getDuration() {
+		return duration;
 	}
 	
 	public void setName(String name) {
@@ -27,11 +78,34 @@ public class Process implements Prioriser{
 	
 	public String toString() {
 		return "Name: " + name + 
-				", Priority: " + priority;
+				", Priority: " + priority + 
+				", Arrival: " + arrival + 
+				", Duration: " + duration + 
+				", has I/O: " + hasIO + 
+				", I/O moments: " + arrToString();
 	}
 
-//	@Override
+	@Override
 	public int getPriority() {
 		return priority;
+	}
+	
+	private String arrToString() {
+		StringBuffer str = new StringBuffer();
+		str.append("[");
+		for(int i = 0; i < ioArrivals.length; i++) {
+			str.append(ioArrivals[i] + (i == ioArrivals.length-1 ? "" : ", "));
+		}
+		return str.append("]").toString();
+	}
+	
+	private int[] toIntArray(String[] arr) {
+		int[] newArr = new int[arr.length];
+		
+		for(int i = 0; i < newArr.length; i++) {
+			newArr[i] = Integer.parseInt(arr[i]);
+		}
+		
+		return newArr;
 	}
 }
